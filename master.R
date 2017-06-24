f8767a0151f0b92e5ca4e46b6cd736c77c4fb531
MoisesExpositoAlonso <moisesexpositoalonso@gmail.com>
2017-06-24 19:33:49 +0200

    Merged master.r

--- master.R
+++ master.R
@@ -1,4 +1,11 @@
 
 library(shiny)
+<<<<<<< Updated upstream
 # runApp(".")
 runApp(host="0.0.0.0",port=5050)
+=======
+#runApp(".")
+#runApp(host="0.0.0.0",port=5050)
+runApp(".",host="0.0.0.0",port=3168)
+
+>>>>>>> Stashed changes
